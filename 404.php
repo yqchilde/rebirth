915316748c81225a453c40817e0dddb7497d62b6
yqchilde <yqchilde@gmail.com>
2020-03-01 23:03:04 +0800

    Update 404.php

--- 404.php
+++ 404.php
@@ -17,7 +17,7 @@ get_header();
             <section class="d-flex flex-column justify-content-center align-items-center w-100 error-message">
                 <h1 class="error-code">Error 404</h1>
                 <p class="error-description">Page not found</p>
-                <a class="error-link" href="https://iiong.com/">返回首页</a>
+                <a class="error-link" href="<?php echo home_url(); ?>">返回首页</a>
             </section>
         </div>
     </div>
