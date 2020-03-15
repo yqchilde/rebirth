14af5f3cdd05ea2cebc2b6b9788c6523dd1af5f1
yqchilde <yqchilde@gmail.com>
2020-03-15 15:52:01 +0800

    🉑更新ICP指向链接

--- footer.php
+++ footer.php
@@ -23,7 +23,7 @@
     <div class="container d-flex flex-wrap-reverse justify-content-md-between justify-content-center text-center main-footer-audit">
         <p>
 			<?php if ( rebirth_option( 'site_icp' ) ) : ?>
-                <a href="http://www.miitbeian.gov.cn" target="_blank"
+                <a href="http://www.beian.miit.gov.cn" target="_blank"
                    rel="nofollow noopener"><?php echo rebirth_option( 'site_icp' ) ?>&nbsp;</a>
 			<?php endif; ?>
 
